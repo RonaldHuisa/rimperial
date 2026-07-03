425ffbd5a7accbb1bbbce8076c7f8cd99e224f01
Ronald Huisa <119126048+RonaldHuisa@users.noreply.github.com>
2026-07-03 11:40:00 -0500

    Delete frontend/src directory

--- frontend/src/routes/contentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getSupportChannels, listPublishedArticles, getPublishedArticle } = require("../controllers/contentController");
-const router = express.Router();
-router.get("/support/channels", getSupportChannels);
-router.get("/news", listPublishedArticles);
-router.get("/news/:slug", getPublishedArticle);
-module.exports = router;
