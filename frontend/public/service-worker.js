12c139dd29ca747a342e1dd48d50d689a0a556e0
Ronald Huisa <119126048+RonaldHuisa@users.noreply.github.com>
2026-07-03 11:39:53 -0500

    Delete frontend/public directory

--- frontend/public/service-worker.js
+++
@@ -1,3 +0,0 @@
-const CACHE_NAME = "royal-imperial-ai-v100";
-self.addEventListener("install", (event) => { self.skipWaiting(); });
-self.addEventListener("activate", (event) => { event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))); self.clients.claim(); });
